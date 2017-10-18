ffc16330c404ceafc85046757dd71a62fe655179
José Pedro Andrés García <macklus@debianitas.net>
2017-10-18 15:36:11 +0200

    * Delete unused trait

--- traits/DataTrait.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace macklus\yii2-geotypeahead\traits;
-
-trait DataTrait {
-	protected function getValidCountries() {
-		return [ "AD","AR","AS","AT","AU","AX","BD","BE","BG","BR","BY","CA","CH","CO","CR","CZ","DE","DK","DO","DZ","ES","FI","FO","FR","GB","GF","GG","GL","GP","GT","GU","HR","HU","IE","IM","IN","IS","IT","JE","JP","LI","LK","LT","LU","MC","MD","MH","MK","MP","MQ","MT","MX","MY","NC","NL","NO","NZ","PH","PK","PL","PM","PR","PT","RE","RO","RU","SE","SI","SJ","SK","SM","TH","TR","US","VA","VI","WF","YT","ZA"];
-	}
-}
